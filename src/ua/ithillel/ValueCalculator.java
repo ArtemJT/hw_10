81e7b4ff6d2562395806c6237d607679dcb9c33e
Artem Kovalov <artemkov1990@gmail.com>
2022-09-27 22:47:15 +0300

    Second Commit

--- src/ua/ithillel/ValueCalculator.java
+++ src/ua/ithillel/ValueCalculator.java
@@ -4,18 +4,18 @@ import java.util.Arrays;
 import java.util.concurrent.*;
 
 public class ValueCalculator {
+    private static final int SIZE = 2_000_000;
+    private static final int HALF_SIZE = SIZE / 2;
+    private static final float[] array = new float[SIZE];
 
     static void doCalc() {
-        int size = 2_000_000;
-        int halfSize = size / 2;
-        float[] array = new float[size];
-        float[] arr1 = new float[halfSize];
-        float[] arr2 = new float[halfSize];
+        float[] arr1 = new float[HALF_SIZE];
+        float[] arr2 = new float[HALF_SIZE];
 
         long start = System.currentTimeMillis();
         Arrays.fill(array, 3);
-        System.arraycopy(array, 0, arr1, 0, halfSize);
-        System.arraycopy(array, halfSize, arr2, 0, halfSize);
+        System.arraycopy(array, 0, arr1, 0, HALF_SIZE);
+        System.arraycopy(array, HALF_SIZE, arr2, 0, HALF_SIZE);
 
         ExecutorService executor = Executors.newFixedThreadPool(2);
 
@@ -25,24 +25,23 @@ public class ValueCalculator {
         FutureTask<float[]> futureTask1 = new FutureTask<>(callable1);
         FutureTask<float[]> futureTask2 = new FutureTask<>(callable2);
 
-        while (true) {
+        executor.execute(futureTask1);
+        executor.execute(futureTask2);
+
+        while (!(futureTask1.isDone() && futureTask2.isDone())) {
             try {
-                if (futureTask1.isDone() && futureTask2.isDone()) {
-                    arr1 = futureTask1.get();
-                    arr2 = futureTask2.get();
-                    System.out.println("All Tasks Done!");
-                    executor.shutdown();
-                    break;
-                }
-                executor.execute(futureTask1);
-                executor.execute(futureTask2);
+                arr1 = futureTask1.get();
+                arr2 = futureTask2.get();
+                System.out.println("All Tasks Done!");
+                executor.shutdown();
+                break;
             } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
             }
         }
 
-        System.arraycopy(arr1, 0, array, 0, halfSize);
-        System.arraycopy(arr2, 0, array, halfSize, halfSize);
+        System.arraycopy(arr1, 0, array, 0, HALF_SIZE);
+        System.arraycopy(arr2, 0, array, HALF_SIZE, HALF_SIZE);
 
         long duration = System.currentTimeMillis() - start;
         System.out.println("Method duration: " + duration + " ms");
